732869be0a8f123554dae313e1f54fa9ae921281
Candra Julius <candrajulius97@gmail.com>
2022-07-29 12:42:20 +0700

    cors OPTION config

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
   origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   preflightContinue: true,
   optionsSuccessStatus: 204
 }))
